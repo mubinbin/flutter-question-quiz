0a4bd946ca6bb17180daddd62a467a982a70144b
Binbin Mu <binbinmu8@gmail.com>
2021-11-14 19:54:11 -0600

    added buttons, unit tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Text("Hello, this is Mu"));
+    const _questions = [
+      "What's your favorite color?",
+      "What's your favorite car?",
+      "How are you doing?"
+    ];
+
+    return MaterialApp(
+      // Scaffold gives basic app design
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My first app!"),
+        ),
+        // Scaffold's body only takes one widget
+        body: Column(
+          children: [
+            Text(_questions[0]),
+            ElevatedButton(
+              onPressed: () => print("This is answer 1"),
+              child: const Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: () => print("This is answer 2"),
+              child: const Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: () => print("This is answer 3"),
+              child: const Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,35 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:first_app/main.dart';
 
 void main() {
-  testWidgets('render Hello, this is Mu', (WidgetTester tester) async {
+  testWidgets('render appBar', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('Hello, this is Mu'), findsOneWidget);
-    expect(find.text('Hello, this is Binbin'), findsNothing);
+    expect(find.byType(AppBar), findsOneWidget);
+    expect(find.text('My first app!'), findsOneWidget);
+  });
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+  testWidgets('render the first question and answers',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
 
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    expect(find.text('What\'s your favorite color?'), findsOneWidget);
+    expect(find.text('What\'s your favorite car?'), findsNothing);
+    expect(find.text('Answer 1'), findsOneWidget);
+    expect(find.text('Answer 4'), findsNothing);
+    expect(find.text('Answer 2'), findsOneWidget);
+    expect(find.text('Answer 3'), findsOneWidget);
+  });
+
+  testWidgets('tapping buttons', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Answer 1'));
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Answer 2'));
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Answer 3'));
+    expect(find.text('Answer 4'), findsNothing);
   });
 }
