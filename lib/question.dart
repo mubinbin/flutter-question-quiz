871c6846f596768d9deebab6654ffeb687faa926
Binbin Mu <dinobunmu@gmail.com>
2022-02-19 20:36:05 -0600

    updated code based on lint

--- lib/question.dart
+++ lib/question.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   final String _question;
+
   const Question(this._question, Key? key) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
